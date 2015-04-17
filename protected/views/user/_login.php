87cbaacbad16e0af683dcbd22fa4571ca70cd768
fprotano <fprotano@yahoo.com>
2015-04-17 18:08:53 +0200

    aggiunte chiave per password reset

--- protected/views/user/_login.php
+++ protected/views/user/_login.php
@@ -44,7 +44,7 @@
 					<div class="col-sm-3 col-sm-offset-1 social-login">
 						<p><?=Yii::t('app','body.already_registered');?></p>
 						<div class="social-login-buttons">
-							<a href="#" class="btn-01"><?=Yii::t('app','body.buttons.activate');?></a>
+							<a href="#" class="btn-01"><?=Yii::t('app','buttons.activate');?></a>
 							<a href="#" class="btn-02"><?=Yii::t('app','buttons.reset_password');?></a>
 						</div>
 					</div>
