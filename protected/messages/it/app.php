9196305371ac6b8582ec4212985adadda6a2c8aa
fprotano <fprotano@yahoo.com>
2015-04-17 14:43:07 +0200

    creato cartella messages

---
+++ protected/messages/it/app.php
@@ -0,0 +1,49 @@
+<?php
+
+return array (
+
+    
+    'body.register_success' => 'Congratulazioni ti sei registrato sul sito. In questo istante ti è stata inviata una mail con le informazioni necessarie per attivare il tuo account.',
+    'body.activation_success' => 'Congratulazioni in questo momento il tuo account è attivo. Entra nella tua area riservata e inizia a utlizzare i servizi che ti abbiamo riservato.  ',
+    
+    'labels.kindergarten' => 'Asilo Nido',
+    'links.free_register' => 'Registrati gratis',
+    'links.activate_account' => 'Attiva il tuo account',
+    'links.login' => 'Entra nell\'area riservata',
+    'buttons.activate' => 'Attiva account',
+    'buttons.free_register' => 'Registrati gratis',
+    'buttons.register' => 'Registrati',
+    'buttons.login' => 'Login',
+    
+    'models.user.createdAt' => 'Data registrazione',
+    'models.user.lastModifiedAt' => 'Ultima modifica',
+    'models.user.lastLoggedAt' => 'Ultima login',
+    'models.user.profileId' => 'Profilo',
+    'models.user.activationCode' => 'Codice di attivazione',
+    'models.user.email' => 'Email',
+    'models.user.password' => 'Password',
+    'models.user.enabled' => 'Abilitato',
+    'models.user.registeredFromWeb' => 'Registratazione dal web',
+    'models.user.name' => 'Nome',
+    'models.user.surname' => 'Cognome',
+    'models.user.alternativeEmail' => 'Email alternativa',
+    'models.user.questionId' => 'Domanda',
+    'models.user.answer' => 'Risposta',
+    'models.user.regionId' => 'Regione',
+    'models.user.provinceCode' => 'Provincia',
+    'models.user.cityId' => 'Città',
+    'models.user.address' => 'Indirizzo',
+    'models.user.site' => 'Sito web',
+    
+    'title.activation' => 'Attivazione Account',
+    'title.activation_success' => 'Attivazione Conclusa',
+    'title.login' => 'Login',
+    'title.register_babysitter' => 'Registrazione Babysitter',
+    'title.register_kindergarten' => 'Registrazione Asilo Nido',
+    'title.register_parent' => 'Registrazione Famiglie',
+    'title.register_success' => 'Registrazione Conclusa',
+    
+    
+    
+
+);
