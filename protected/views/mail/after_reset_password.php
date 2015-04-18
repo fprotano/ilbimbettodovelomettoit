625f0cb3c42ef7376614c0948b6fb6eda91ef456
fprotano <fprotano@yahoo.com>
2015-04-18 08:03:07 +0200

    Aggiunta gestione reset password

---
+++ protected/views/mail/after_reset_password.php
@@ -0,0 +1,30 @@
+<html>
+        <head>
+        
+        </head>
+        <body>
+
+        Nuova password
+        <br/>
+
+        <?=$bean->newPassword?>
+        Lo staff
+        
+        <br/>
+        <br/>
+        *************************************************************************
+         <br/>
+        <br/>
+        Questa mail è stata generata da un sistema automatico, pertanto non è necessario rispondervi
+        <br/>
+        <br/>
+        *************************************************************************
+        
+        
+        
+        
+        
+        
+            
+        </body>
+</html>
