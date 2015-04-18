1395e2f4d7128044f3e8c5eafddf5067bcde0779
fprotano <fprotano@yahoo.com>
2015-04-18 16:40:36 +0200

    aggiunto nuovo logo

--- protected/views/shared/default_menu.php
+++ protected/views/shared/default_menu.php
@@ -1,6 +1,7 @@
 <nav id="mainmenu" class="mainmenu">
 <ul>	
-    <li class="logo-wrapper"><a href="<?php echo Yii::app()->request->baseUrl; ?>/">Il<span class="green">bimbetto</span><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logoibdlm.png" />dove<i>lo<span class="green">metto</span></i>?</a></li>
+    <li class="logo-wrapper"><a href="<?php echo Yii::app()->request->baseUrl; ?>/"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logoibdlm.png" /></a></li>
+<!--    <li class="logo-wrapper"><a href="<?php echo Yii::app()->request->baseUrl; ?>/">Il<span class="green">bimbetto</span><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logoibdlm.png" />dove<i>lo<span class="green">metto</span></i>?</a></li>-->
 						<li class="active"><a href="<?php echo Yii::app()->request->baseUrl; ?>/"><?=Yii::t('app','menu.home')?></a></li>
 						<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/site/about"><?=Yii::t('app','menu.aboutus')?></a></li>
 						<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/site/contacts"><?=Yii::t('app','menu.contacts')?></a></li>
