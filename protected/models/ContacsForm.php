def2c340418cedc68404c1907a2cd5a54a06271b
fprotano <Mauro@Mauro-PC>
2015-04-19 17:48:42 +0200

    aggiunto gestione form contatti

---
+++ protected/models/ContacsForm.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ContacsForm
+ *
+ * @author Mauro
+ */
+class ContacsForm extends CFormModel
+{
+	public $name;
+	public $email;
+	public $subject;
+	public $body;
+	public $verifyCode;
+
+        /**
+	 * Declares the validation rules.
+	 */
+	public function rules()
+	{
+		return array(
+			// name, email, subject and body are required
+			array('name, email, subject, body', 'required'),
+			// email has to be a valid email address
+			array('email', 'email'),
+			// verifyCode needs to be entered correctly
+			array('verifyCode', 'captcha', 'allowEmpty'=>!CCaptcha::checkRequirements()),
+		);
+	}
+
+	/**
+	 * Declares customized attribute labels.
+	 * If not declared here, an attribute would have a label that is
+	 * the same as its name with the first letter in upper case.
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'verifyCode'=>'Verification Code',
+		);
+	}
+}
