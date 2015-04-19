1e7094bb321802562a2caf3c4117e94e250d3e2a
fprotano <Mauro@Mauro-PC>
2015-04-19 18:13:11 +0200

    aggiunto gestione form contatti

--- protected/views/shared/default_menu.php
+++ protected/views/shared/default_menu.php
@@ -4,7 +4,7 @@
 <!--    <li class="logo-wrapper"><a href="<?php echo Yii::app()->request->baseUrl; ?>/">Il<span class="green">bimbetto</span><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logoibdlm.png" />dove<i>lo<span class="green">metto</span></i>?</a></li>-->
 						<li class="active"><a href="<?php echo Yii::app()->request->baseUrl; ?>/"><?=Yii::t('app','menu.home')?></a></li>
 						<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/site/about"><?=Yii::t('app','menu.aboutus')?></a></li>
-						<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/site/contacts"><?=Yii::t('app','menu.contacts')?></a></li>
+						<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/contacts"><?=Yii::t('app','menu.contacts')?></a></li>
 						<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/site/register"><?=Yii::t('app','menu.register')?></a></li>
                                                 
 </ul>
