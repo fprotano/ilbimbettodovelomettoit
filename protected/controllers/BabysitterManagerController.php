bb3491cef0d7174aa842441fa33145e7e7068614
fprotano <fprotano@yahoo.com>
2015-04-19 10:42:00 +0200

    Aggiunti controller aree riservata

---
+++ protected/controllers/BabysitterManagerController.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of BabysitterManagerController
+ *
+ * @author francesco
+ */
+class BabysitterManagerController extends BaseFrontController
+{
+    
+     /**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+//			'postOnly + delete', // we only allow deletion via POST request
+		);
+	}
+    /**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+            
+                $allowed = YII::app()->session["userEmail"];
+                
+                if($allowed==""){
+                    
+                        return array(
+
+                            array('deny',  // deny all users
+                                    'users'=>array('*'),
+                            ),
+
+                    );
+                    
+                }
+                    
+               
+		return array(
+			array('allow',  // allow all users to perform 'index' and 'view' actions
+				'actions'=>array('index'),
+				'users'=>array($allowed),
+			),
+	
+		);
+	}
+
+        
+    public function actionIndex()
+       {
+            $this->layout="private";
+            $this->render('index');
+	}
+        
+      
+}
+
