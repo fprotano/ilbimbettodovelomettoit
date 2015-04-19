7b772e980bb1ba730e2556bd656ce1911627b6d5
fprotano <Mauro@Mauro-PC>
2015-04-19 18:15:00 +0200

    aggiunto gestione form contatti

--- protected/views/shared/user_menu.php
+++ protected/views/shared/user_menu.php
@@ -4,7 +4,7 @@
 <li class="logo-wrapper"><a href="<?php echo Yii::app()->request->baseUrl; ?>/"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logoibdlm.png" /></a></li>
 <li class="active"><a href="<?php echo Yii::app()->request->baseUrl; ?>/dashboard">Dashboard</a></li>
 <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/site/about">Chi siamo</a></li>
-<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/site/contacts">Contatti</a></li>
+<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/contacts">Contatti</a></li>
 <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/user/logout">Esci</a></li>
 </ul>
 </nav>
